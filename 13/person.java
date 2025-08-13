880d20bfdd6cd0b633bdde1c91d0351676d3f49b
timt-212 <tamluktimt@gmail.com>
2025-08-13 12:27:47 +0530

    java

---
+++ 13/person.java
@@ -0,0 +1,12 @@
+public class person {
+    private String firstname;
+    private String lastname;
+    private int age;
+    public person(){
+    }
+    public person(String firstname,String lastname,int age){
+        this.firstname=firstname;
+        this.lastname=lastname;
+        this.age=age;
+    }
+}
